421c0c4c1680eeb04ff5ccf7c2594aaf8940a003
Reena <reenasai84@gmail.com>
2026-05-17 20:34:01 +0530

    added files

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   fullyParallel: true, 
   // retries: process.env.CI ? 2 : 0, 
   // retries:1, 
-  //workers: process.env.CI ? 1 : undefined, 
-  workers: 2, 
+  // workers: process.env.CI ? 1 : undefined, 
+  // workers: 2, 
 
   reporter: [ 
   ['html', {outputFolder: '../reports/html-report'}],
